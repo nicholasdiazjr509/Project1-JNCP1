fa6cdd4bf19577f66ec1c69baf9d6a6975b3e8ed
Nicholas Diaz Jr <nicholasdiazjr509@gmail.com>
2022-10-17 15:50:46 -0500

    Initial commit

---
+++ src/main/java/com/company/Summative1CristieJBNicholas/Summative1CristieJbNicholasApplication.java
@@ -0,0 +1,13 @@
+package com.company.Summative1CristieJBNicholas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative1CristieJbNicholasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative1CristieJbNicholasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/Summative1CristieJBNicholas/Summative1CristieJbNicholasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Summative1CristieJBNicholas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative1CristieJbNicholasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
